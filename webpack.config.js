3bb69ac8e1182e1039abd1820ea11efbbde3c542
LuWo0 <loviedo46@yahoo.com>
2022-08-23 16:49:17 -0700

    Added SASS loader, override Bootstrap colors

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader","css-loader"],
-            }
-        ]
-    }
-    
-}
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",   //3. Inject styles into DOM
+          "css-loader",     //2. Turns css into common javascript
+          "sass-loader",    //1. Turns sass into css
+        ],
+      },
+    ],
+  },
+};
